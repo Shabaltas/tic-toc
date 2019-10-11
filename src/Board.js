9cf2678b75eba980a0c2598d3a71744d2da9f3be
Angelina <lina.shabaltas@mail.ru>
2019-10-11 14:51:32 +0300

    Add files via upload

---
+++ src/Board.js
@@ -0,0 +1,92 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function calculateWinner(squares) {
+    const lines = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+        const [a, b, c] = lines[i];
+        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+            return squares[a];
+        }
+    }
+    return null;
+}
+
+function Square(props) {
+    return <button className="square" onClick={props.onClick}>
+                {props.value}
+            </button>
+}
+class Board extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            squares: Array(9).fill(null),
+            xIsNext: true,
+            done: false,
+            status: 'Next X',
+        };
+    }
+
+    handleClick(i) {
+        if (this.state.squares[i] || this.state.done) return;
+        const squares = this.state.squares.slice();
+        squares[i] = (this.state.xIsNext ? 'X' : 'O');
+        this.setState(calculateWinner(squares) ? {
+            squares: squares,
+            xIsNext: !this.state.xIsNext,
+            done: true,
+        } : {
+            squares: squares,
+            xIsNext: !this.state.xIsNext,
+            done: false,
+        });
+    }
+
+    renderSquare(i) {
+        return <Square value={this.state.squares[i]}
+                        onClick={() => this.handleClick(i)}/>;
+    }
+
+    renderStatus() {
+        return <div className="status">{this.state.done
+            ? 'Winner is '.concat(this.state.xIsNext ? 'O' : 'X')
+            : 'Next is '.concat(this.state.xIsNext ? 'X' : 'O')}</div>
+    }
+
+    render() {
+        return (
+            <div>
+                {this.renderStatus()}
+                <div className="board-row">
+                    {this.renderSquare(0)}
+                    {this.renderSquare(1)}
+                    {this.renderSquare(2)}
+                </div>
+                <div className="board-row">
+                    {this.renderSquare(3)}
+                    {this.renderSquare(4)}
+                    {this.renderSquare(5)}
+                </div>
+                <div className="board-row">
+                    {this.renderSquare(6)}
+                    {this.renderSquare(7)}
+                    {this.renderSquare(8)}
+                </div>
+            </div>
+        );
+    }
+
+}
+
+export default Board;
